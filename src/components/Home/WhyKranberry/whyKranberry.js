90fa25fde873c5429d23829387083e6f0d30e705
vaishu-shann <ilukiabi@gmail.com>
2024-09-03 22:55:05 +0530

    why Kranberry section - created

---
+++ src/components/Home/WhyKranberry/whyKranberry.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./whyK.css"
+
+const WhyKranberry = () => {
+  return (
+    <div className='why-section'>
+        <div className='why-sec-width'>
+      <div className='why-sec-layer-1'>
+        <div className='why-header'>
+        Why Work With Kranberry?
+        </div>
+        <div className='why-para'>
+        We're problem solvers. We're revenue specialists. We're design, content, and integration experts who will leverage the HubSpot platform to attract more customers, convert more leads, and achieve sustainable growth for your business.</div>
+      </div>
+      <div className='why-sec-cards'>
+        <div className='why-single-card'>
+            <div className='why-card-head'>Custom Integrations</div>
+            <div className='why-card-desc'>No matter your existing platform, we seamlessly onboard, migrate, configure, and integrate all your systems into a singular, supercharged revenue engine.</div>
+            <div className='why-card-cta'>Learn More About Custom Integrations</div>
+
+        </div>
+        <div className='why-single-card'>
+            <div className='why-card-head'>Growth-Driven Design</div>
+            <div className='why-card-desc'>We reimagine and reinvent your website, landing pages, and collateral into stunning, engaging, conversion-focused lead generators.</div>
+            <div className='why-card-cta'>Explore Web Design Solutions</div>
+
+        </div>
+        <div className='why-single-card'>
+            <div className='why-card-head'>Strategic Content</div>
+            <div className='why-card-desc'>Our award-winning content engineers maximize inbound strategy to amplify your message, build meaningful relationships and skyrocket conversions.</div>
+            <div className='why-card-cta'>Tell Me More About Strategic Content Services</div>
+
+        </div>
+      </div>
+      
+      </div>
+    </div>
+  )
+}
+
+export default WhyKranberry
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "../styles/homePage.css"
 import HomeBanner from '../components/Home/HomeBanner/HomeBanner'
+import WhyKranberry from '../components/Home/WhyKranberry/whyKranberry'
 
 const HomePage = () => {
   return (
-    <div className='home-screen'>
+    <div >
       <HomeBanner/>
+      <WhyKranberry/>
     </div>
   )
 }
