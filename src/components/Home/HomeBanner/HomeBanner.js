eea4736d99dd2213e9041bdd8b38dcf41340d58e
vaishu-shann <ilukiabi@gmail.com>
2024-09-11 21:36:14 +0530

    Home screen animation

--- src/components/Home/HomeBanner/HomeBanner.js
+++ src/components/Home/HomeBanner/HomeBanner.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./homeBanner.css"
-import HomeVector from "../../../assets/images/hero-alt-img.jpeg"
+import HomeVector from "../../../assets/images/hero-img2.png"
 
 
 const HomeBanner = () => {
@@ -9,11 +9,31 @@ const HomeBanner = () => {
             <div className='homepage-banner'>
                 <div className='h-b-left'>
                     <div className='sub-head heading4'>Design. Build. Integrate. Scale.</div>
-                    <div className='changing-head'>Build your Business on the HubSpot Platform.</div>
+                    {/* <div class="container">
+                        <div class="title">
+                            <div class="word rhythm">
+                            <span className='changing-head'>Build your Business </span>
+                            </div>
+                            <div class="word city">
+                            <span className='changing-head'>Scale your Business </span>
+                            </div>
+                            <div class="word voice">
+                            <span className='changing-head'>Design your Business </span>
+                            </div>
+                        </div>
+                    </div> */}
+
+                    <div class="wrap">
+                        <p class="flip"><span className='changing-head'>Build your Business</span>
+                            <span className='changing-head'>Scale your Business</span>
+                            <span className='changing-head'>Design your Business</span>
+                        </p>
+                    </div>
+                    <div className='changing-subhead'> on the HubSpot Platform.</div>
                     <div className='btn light'></div>
                 </div>
                 <div className='h-b-right'>
-                   <img src={HomeVector} alt='home-illustration' className='home-illustration' />
+                    <img src={HomeVector} alt='home-illustration' className='home-illustration' />
                 </div>
             </div>
         </>
