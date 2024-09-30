b6c0f46171f873cf7147bf7fea9f3912d45ff9b0
madhavsethi <85222723+madhavsethi@users.noreply.github.com>
2024-09-30 10:53:42 +0530

    Update DetailHome.js

--- src/components/DetailPortfolio/DetailHome/DetailHome.js
+++ src/components/DetailPortfolio/DetailHome/DetailHome.js
@@ -7,8 +7,28 @@ const DetailHome = () => {
     <>
       <div className='d-home-banner'>
         <div className='d-home-cont'>
-          <div className='d-home-head'>Canidium</div>
-          <div className='d-home-desc'>Canidium is a leading sales performance management firm dedicated to helping business leaders surpass their revenue goals through strategic consulting and technology solutions. Specializing in Sales Performance Management (SPM), Pricing, Configure Price Quote (CPQ), Sales Consulting, and Software Implementation and Setup, Canidium provides comprehensive services designed to optimize business performance. With decades of expertise, Canidium's solution architects and strategists are equipped to tackle complex challenges and drive success in competitive markets.</div>
+          <div className='d-home-head'>Sun Life - Insurance Application</div>
+          <div className='d-home-desc'>Overview of Sun Life:
+Sun Life is a prominent financial services organization serving millions of clients across Canada, the U.S., Asia, the U.K., and other regions. With a strong commitment to helping clients navigate life's important moments, Sun Life offers a comprehensive suite of protection and wealth products and services. Their offerings include:
+
+Insurance Products: Life, health, wellness, disability, critical illness, stop-loss, and long-term care insurance.
+
+Investment Solutions: Mutual funds, segregated funds, annuities, and guaranteed investment products.
+
+Advisory Services: Financial and retirement planning services tailored to individual and business needs.
+
+Asset Management: Pooled funds, institutional portfolios, and pension funds.
+
+Commitment to Wellness:
+Beyond financial services, Sun Life is dedicated to promoting health and wellness within communities through initiatives such as:
+
+Team Up Against Diabetes: A significant investment supporting diabetes awareness, prevention, care, and research globally.
+
+Health-Screening Clinics: Programs to facilitate early detection and management of health conditions.
+
+Workplace Peer-Support Programs: Initiatives to foster supportive environments in workplaces.
+
+Disease-Prevention Programs: Strategies aimed at promoting healthy living and preventing illness.</div>
         </div>
       </div>
       <div className='port-fit-img'>
